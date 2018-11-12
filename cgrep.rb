2f49581f9e17c6aa195386f5a141d7d820a07db5
Alex Rocha <alex.rochas@yahoo.com.br>
2018-11-12 16:37:59 -0200

    feat: pass patterns by options

--- cgrep.rb
+++ cgrep.rb
@@ -1,19 +1,28 @@
 #!/usr/bin/env ruby
 require 'rainbow'
+require 'optparse'
+
+options = {}
+OptionParser.new do |opts|
+  opts.banner = "Usage: --pattern PATTERN:COLOR [text]"
+
+  opts.on("-p", "--pattern PATTERN:COLOR", "Pattern and respective color") do |p|
+    options[:patterns] ||= Array.new
+    key, value = p.split(':')
+    options[:patterns] << {key => value}
+  end
+end.parse!
 
 userInput = ARGV.join(' ')
 
-expecs = [
-  {'red' => 'yellow'},
-  {'green' => 'blue'}
-]
+expecs = options[:patterns]
 
 _return = []
 
 expecs.each do |expec|
   pattern, color = expec.to_a.first
-  if Regexp.new(pattern) =~ userInput
-    userInput.gsub! pattern, Rainbow(pattern).send(color)
+  if match = userInput.match(Regexp.new(pattern))
+    userInput.gsub! match.to_s, Rainbow(match).send(color)
   end
 end
 
