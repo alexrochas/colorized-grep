c8a044df150eea8287986cd3090632916607930b
Alex Rocha <alex.rochas@yahoo.com.br>
2018-11-12 20:50:46 -0200

    feat: add std input feature

--- cgrep.rb
+++ cgrep.rb
@@ -2,30 +2,41 @@
 require 'rainbow'
 require 'optparse'
 
-options = {}
+@options = {}
 OptionParser.new do |opts|
   opts.banner = "Usage: --pattern PATTERN:COLOR [text]"
 
   opts.on("-p", "--pattern PATTERN:COLOR", "Pattern and respective color") do |p|
-    options[:patterns] ||= Array.new
+    @options[:patterns] ||= Array.new
     key, value = p.split(':')
-    options[:patterns] << {key => value}
+    @options[:patterns] << {key => value}
+  end
+
+  opts.on("-i", "--input", "Standard input") do |i|
+    @options[:stdInput] = i
   end
 end.parse!
 
-userInput = ARGV.join(' ')
+def colorize userInput
+  expecs = @options[:patterns]
 
-expecs = options[:patterns]
+  _return = []
 
-_return = []
-
-expecs.each do |expec|
-  pattern, color = expec.to_a.first
-  if match = userInput.match(Regexp.new(pattern))
-    userInput.gsub! match.to_s, Rainbow(match).send(color)
+  expecs.each do |expec|
+    pattern, color = expec.to_a.first
+    if match = userInput.match(Regexp.new(pattern))
+      userInput.gsub! match.to_s, Rainbow(match).send(color)
+    end
   end
+
+  userInput
 end
 
-puts userInput
-
+if @options[:stdInput]
+  while line = $stdin.gets
+    puts colorize(line)
+  end
+else
+  puts colorize(ARGV.join(' '))
+end
 
