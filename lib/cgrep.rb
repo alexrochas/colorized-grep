b98c016b555d0386c7b39f5ba8cbee6ce13721ef
Alex Rocha <alex.rochas@yahoo.com.br>
2018-11-14 14:18:23 -0200

    fix: rubocop errors

--- lib/cgrep.rb
+++ lib/cgrep.rb
@@ -1,42 +1,41 @@
-#!/usr/bin/env ruby
 require 'rainbow'
 require 'optparse'
 
 @options = {}
 OptionParser.new do |opts|
-  opts.banner = "Usage: --pattern PATTERN:COLOR [text]"
+  opts.banner = 'Usage: --pattern PATTERN:COLOR [text]'
 
-  opts.on("-p", "--pattern PATTERN:COLOR", "Pattern and respective color") do |p|
-    @options[:patterns] ||= Array.new
-    key, value = p.split(':')
-    @options[:patterns] << {key => value}
-  end
+  opts
+    .on('-p', '--pattern PATTERN:COLOR', 'Pattern and respective color') do |p|
+      @options[:patterns] ||= []
+      key, value = p.split(':')
+      @options[:patterns] << { key => value }
+    end
 
-  opts.on("-i", "--input", "Standard input") do |i|
+  opts.on('-i', '--input', 'Standard input') do |i|
     @options[:stdInput] = i
   end
 end.parse!
 
-def colorize userInput
+def colorize(user_input)
   expecs = @options[:patterns]
 
   _return = []
 
   expecs.each do |expec|
     pattern, color = expec.to_a.first
-    if match = userInput.match(Regexp.new(pattern))
-      userInput.gsub! match.to_s, Rainbow(match).send(color)
+    if (match = user_input.match(Regexp.new(pattern)))
+      user_input.gsub! match.to_s, Rainbow(match).send(color)
     end
   end
 
-  userInput
+  user_input
 end
 
 if @options[:stdInput]
-  while line = $stdin.gets
+  while (line = $stdin.gets)
     puts colorize(line)
   end
 else
   puts colorize(ARGV.join(' '))
 end
-
